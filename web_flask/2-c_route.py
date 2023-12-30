67e0af830b3bbe092a65b771e39a3ad11fa4baf0
DAKUBZ <tinekmarty@gmail.com>
2023-12-30 11:28:31 +0000

    print text

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,39 +1,34 @@
 #!/usr/bin/python3
-from flask import Flask
-"""class Flask"""
+""" mport Flask module form flask
+python script that display “Hello HBNB!”
+and /hbnb: display "HBNB"
+/c/<text>: display “C ” followed by the value of the text variable
+"""
 
+from flask import Flask
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello_hbnb():
-    """displays text
-    Returns:
-        text
-    """
-    return "Hello HBNB!"
+    """ Returns text. """
+    return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
-def display_hbnb():
-    """displays text
-    Returns:
-        text
-    """
-    return "HBNB"
+@app.route('/hbnb')
+def hbnb():
+    """ Return text. """
+    return 'HBNB'
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def display_text(text):
-    """displays text
-    Args:
-        text (str): text
-    Returns:
-        text
-    """
-    return 'C %s' % text.replace('_', ' ')
+@app.route('/c/<text>')
+def c_text(text):
+    """ replace text with variable. """
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
